48673f8f2359b73a1eb29d0245f57c4907e921a7
Brandon Trautmann <btrautmann@users.noreply.github.com>
2021-10-28 10:40:06 -0400

    Remove some cruft that's not necessary

--- example_package/lib/example_package.dart
+++ example_package/lib/example_package.dart
@@ -1,11 +1,8 @@
 library example_package;
 
-import 'package:mocktail/mocktail.dart';
-
-class MyRealMock extends Mock {}
 /// A Calculator.
 class Calculator {
-  final stringLiteral = 'but what';
+  final stringLiteral = 'bu what';
 
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
--- example_package/test/example_package_test.dart
+++ example_package/test/example_package_test.dart
@@ -1,9 +1,4 @@
-import 'package:mocktail/mocktail.dart';
 import 'package:test/scaffolding.dart';
-
-class MyMock extends Mock {
-
-}
 
 void main() {
   
